21a375e6c9c888093ddb03f9de89e588a3b1c648
Sam Tarver <starver@hotmail.co.uk>
2025-11-02 15:57:26 +0000

    some progress

--- src/main.rs
+++ src/main.rs
@@ -12,52 +12,56 @@ use clap::{Parser, ValueEnum};
 use walkdir::WalkDir;
 
 const THREAD_COUNT: usize = 1;
+const MUSIC_EXTENSIONS: [&str; 2] = ["mp3", "flac"];
 
 fn main() {
     let args = Args::parse();
 
     let (sender, receiver) = mpmc::sync_channel(1);
 
-    let workers = (0..THREAD_COUNT).map(|_| {
-        let rx = receiver.clone();
-        thread::spawn(move || worker(rx))
-    });
+    let workers: Vec<_> = (0..THREAD_COUNT)
+        .map(|_| {
+            let rx = receiver.clone();
+            thread::spawn(move || worker(rx, args.encoding))
+        })
+        .collect();
 
     producer(sender.clone(), &args.in_folder, &args.out_folder);
     drop(sender);
 
-    workers.for_each(|w| {
+    workers.into_iter().for_each(|w| {
         w.join().unwrap();
         println!("Joined worker");
     });
 }
 
-fn process_file(in_file: &Path, out_file: &Path) {
+fn process_file(in_file: &Path, out_file: &Path, encoding: Encoding) {
     println!("in: {:?}, out: {:?}", in_file, out_file)
 }
 
 fn producer(sender: mpmc::Sender<(PathBuf, PathBuf)>, in_folder: &str, out_folder: &str) {
+    let in_prefix = PathBuf::from(in_folder);
+    let out_prefix = PathBuf::from(out_folder);
     for entry in WalkDir::new(in_folder) {
         let unw_entry = entry.unwrap();
-        println!("Sender: {:?}", unw_entry);
-        if unw_entry.file_type().is_file() {
-            println!("Sending: {:?}", unw_entry.path());
-            sender
-                .send((
-                    unw_entry.into_path(),
-                    PathBuf::from_str(out_folder).unwrap(),
-                ))
-                .unwrap();
+        if should_process(&unw_entry) {
+            let inf = unw_entry.into_path();
+            let outf = out_prefix.join(inf.strip_prefix(&in_prefix).unwrap());
+            sender.send((inf, outf)).unwrap();
         }
     }
-
-    // sender.send(("inf".into(), "outf".into())).unwrap();
 }
 
-fn worker(receiver: mpmc::Receiver<(PathBuf, PathBuf)>) {
-    let (in_file, out_file) = receiver.recv().unwrap();
-    process_file(&in_file, &out_file);
+fn worker(receiver: mpmc::Receiver<(PathBuf, PathBuf)>, encoding: Encoding) {
+    for (in_file, out_file) in receiver {
+        process_file(&in_file, &out_file, encoding);
+    }
     println!("Worker exited")
+}
+
+fn should_process(file_entry: &walkdir::DirEntry) -> bool {
+    file_entry.path().is_file()
+        && MUSIC_EXTENSIONS.contains(&file_entry.path().extension().unwrap().to_str().unwrap())
 }
 
 #[derive(Parser, Debug)]
